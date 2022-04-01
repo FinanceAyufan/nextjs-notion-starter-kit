e30cf8e8b22277106b8c39131b14007b91d92737
Yuf <101466191+FinanceAyufan@users.noreply.github.com>
2022-04-01 13:53:13 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,23 +1,23 @@
 export default {
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '1ca423ee1edf4014b32974f58478a298',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Ayufanfan',
+  domain: 'https://heathered-harrier-dda.notion.site/1ca423ee1edf4014b32974f58478a298',
+  author: 'Yuf Long',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'Ayufanfan0520',
+  github: 'FinanceAyufan',
+  linkedin: 'Yufan',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
